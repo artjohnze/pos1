ba57e8492b51f75a1f909c69081dd784c1d45f10
Art johnzelle Balubal <artjohnzelle@gmail.com>
2025-08-27 12:41:27 +1000

    first commit

---
+++ pos/main/delete.php
@@ -0,0 +1,18 @@
+<?php
+include('../connect.php');
+$id = $_GET['id'];
+$c = $_GET['invoice'];
+$sdsd = $_GET['dle'];
+$qty = $_GET['qty'];
+$wapak = $_GET['code'];
+//edit qty and qty_sold when deleting sale
+$sql = "UPDATE products 
+			SET qty=qty+?, qty_sold=qty_sold-?
+			WHERE product_id=?";
+$q = $db->prepare($sql);
+$q->execute(array($qty, $qty, $wapak));
+
+$result = $db->prepare("DELETE FROM sales_order WHERE transaction_id= :memid");
+$result->bindParam(':memid', $id);
+$result->execute();
+header("location: sales.php?id=$sdsd&invoice=$c");
